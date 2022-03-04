ce9cefedab2d51c6a74b89f1e9831a62b68922b0
HECTOR JAVIER PADILLA LARA <2609@rcarbonifera.tecnm.mx>
2022-03-04 08:17:05 -0600

    Cambios

---
+++ PortalEgresados/wwwroot/JavaScript/General.js
@@ -0,0 +1,12 @@
+﻿var menu = document.querySelector(".contextual");
+
+document.addEventListener("click", function (event) {
+    if (event.target.tagName == "TD" && event.target.parentNode.children.length - 1 == event.target.cellIndex) {
+        event.target.appendChild(menu);
+        menu.style.display = "block";
+    }
+    else if (menu.style.display == "block") {
+        menu.style.display = "none";
+    }
+
+})
